963739545649bcedf4aa31c518c067ed01440110
Husam Jenidi <117489131+Husam-Jenidi@users.noreply.github.com>
2023-08-01 15:58:54 +0200

    initial commit

---
+++ app/src/main/java/com/example/newsapp/OnFetchDataListener.java
@@ -0,0 +1,10 @@
+package com.example.newsapp;
+
+import com.example.newsapp.modules.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse>{
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
---
+++ app/src/main/java/com/example/newsapp/SelectListener.java
@@ -0,0 +1,7 @@
+package com.example.newsapp;
+
+import com.example.newsapp.modules.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+}
